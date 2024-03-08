ab7ac034fe5435444730f7c8e1e9e921fee8161e
chinmay2814 <crmalve2814@gmail.com>
2024-03-08 23:52:05 +0530

    login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,12 +2,15 @@ const express=require("express");
 const router=express.Router();
 
 //api calling
-const {signup}=require("../controllers/authControllers")
+const {signup,login}=require("../controllers/authControllers")
 
 //routes
 
 //signup
 router.post("/signup",signup);
+
+//login
+router.post("/login",login);
 
 module.exports = router;
 
