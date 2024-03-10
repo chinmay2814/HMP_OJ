7fc8c441af44bbded5796f4d28083c4db05a0419
Priyanshu Anand <priyanshuanandd@gmail.com>
2024-03-10 12:48:54 +0530

    Added Google Btn

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -49,6 +49,7 @@ const LoginPage = () => {
     }
   };
 
+  const handleGoogle = async (e) => {};
   return (
     <div className="wrapper">
       <div class="form-box login">
@@ -81,9 +82,13 @@ const LoginPage = () => {
             />
             <label htmlFor="showPasswordCheckbox">Show password</label>
           </div>
-
+          <p> </p>
           <button type="submit" class="btn" onClick={handleSubmit}>
             Login
+          </button>
+          <p> </p>
+          <button type="submit" class="btn2" onClick={handleGoogle}>
+            Login in with Google
           </button>
         </form>
       </div>
